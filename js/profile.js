42f1fb3d896c02c5367262cbcc115e699f7e676e
David <davgam.work@gmail.com>
2024-04-23 14:31:13 +0200

    Profile page main structure was created with some examples and its responsive.

--- js/profile.js
+++ js/profile.js
@@ -17,7 +17,6 @@ document.addEventListener("DOMContentLoaded", (event) => {
     userFirstNameText = document.getElementsByClassName("user-firstname")
     offersOption = document.getElementById("offers-option")
     pastordersOption = document.getElementById("pastorders-option")
-    personalOption = document.getElementById("daily-option")
 
     
     window.addEventListener("load",()=>{ // on window load
@@ -38,43 +37,20 @@ document.addEventListener("DOMContentLoaded", (event) => {
             DeleteAnimation(hr[1])
         }
     })
-    optionButtons[2].addEventListener("mouseover",()=>{HrWidth(hr[2])})
-    optionButtons[2].addEventListener("mouseout",()=>{
-        if (personalOption.style.display != "block") {
-            hr[2].style.visibility = "hidden"
-            DeleteAnimation(hr[2])
-        }
-    })
 
     optionButtons[0].addEventListener("click",()=>{
         offersOption.style.display = "flex"
         pastordersOption.style.display = "none"
-        personalOption.style.display = "none"
 
         hr[1].style.visibility = "hidden"
         DeleteAnimation(hr[1])
-        hr[2].style.visibility = "hidden"
-        DeleteAnimation(hr[2])
     })
     optionButtons[1].addEventListener("click",()=>{
         offersOption.style.display = "none"
         pastordersOption.style.display = "block"
-        personalOption.style.display = "none"
 
         hr[0].style.visibility = "hidden"
         DeleteAnimation(hr[0])
-        hr[2].style.visibility = "hidden"
-        DeleteAnimation(hr[2])
-    })
-    optionButtons[2].addEventListener("click",()=>{
-        offersOption.style.display = "none"
-        pastordersOption.style.display = "none"
-        personalOption.style.display = "block"
-
-        hr[0].style.visibility = "hidden"
-        DeleteAnimation(hr[0])
-        hr[1].style.visibility = "hidden"
-        DeleteAnimation(hr[1])
     })
 })
 
@@ -84,5 +60,5 @@ function FillUserFirstName(name){
         userFirstNameText[e].innerHTML = name
     }
 }
-function HrWidth(e){e.style.animation = "hrWidth 1.5s"; e.style.visibility = "visible"; e.style.width = "60%"}
+function HrWidth(e){e.style.animation = "hrWidth 1.5s"; e.style.visibility = "visible"; e.style.width = "60%";}
 function DeleteAnimation(e){e.style.animation = "none";}
