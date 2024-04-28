2cca19b1f35643cedc924cd96b30349a85750be0
David <davgam.work@gmail.com>
2024-04-28 14:06:43 +0200

    Order page responsive implemented.

--- js/order.js
+++ js/order.js
@@ -2,11 +2,15 @@ let productCategoriesButtons = []
 let productCategoriesHr = []
 let productCategoriesDivs = []
 
+let productCategorySelected
+
 document.addEventListener("DOMContentLoaded",()=>{
     console.log("DOM fully loaded and parsed")
 
     productCategoriesButtons = document.getElementsByClassName("product-categories")[0].getElementsByTagName("button")
     productCategoriesDivs = document.getElementsByClassName("product-categories-divs")[0].children
+
+    productCategorySelected = document.getElementById("current-category")
 
     for (let button = 0; button<productCategoriesButtons.length; button++) {
         productCategoriesHr[button] = productCategoriesButtons[button].getElementsByTagName("hr")[0]
@@ -37,6 +41,21 @@ document.addEventListener("DOMContentLoaded",()=>{
                 }
             }
             
+        })
+        productCategorySelected.addEventListener("change",()=>{
+            productCategoriesDivs[productCategorySelected.value].style.display = "flex"
+
+            for (let e = 0; e<productCategoriesDivs[button].children.length; e++) {
+                productCategoriesDivs[button].children[e].style.display = "flex"
+            }
+            
+            for (let e = 0; e<productCategoriesHr.length; e++) {
+                if (e != productCategorySelected.value) {
+                    productCategoriesDivs[e].style.display = "none"
+                    productCategoriesHr[e].style.animation = "none"
+                    productCategoriesHr[e].style.visibility = "hidden"
+                }
+            }
         })
     }
 })
