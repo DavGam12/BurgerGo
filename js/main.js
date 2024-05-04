fe9e0bd6e69a5047c527326c67e8722bb4b398d9
David <davgam.work@gmail.com>
2024-05-04 14:26:04 +0200

    Several view Changes in the Main Page, deleted the categories in it and changed 'MAKE AN ORDER' button to 'ORDER'. Also, changed the products in the current order in the Order Page. The main automation in the Order Page has been completed.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 let currentDay = new Date().getDay()
-let scheduleButton
 let scheduleParagraphs
 let currentSchedule
 
@@ -8,60 +7,38 @@ document.addEventListener("DOMContentLoaded", (event) => {
     console.log("DOM fully loaded and parsed")
 
     
-    scheduleButton = document.getElementsByClassName("schedule")[0].getElementsByTagName("button")[0]
     scheduleParagraphs = document.getElementsByClassName("schedule")[0].getElementsByTagName("p")
     currentSchedule = document.getElementById("current-schedule")
 
-
-    scheduleButton.addEventListener("click", ()=>{ // to show the whole schedule
-        let schedule = document.getElementsByClassName("schedule-info")[0]
-        if (innerWidth<500) {
-            schedule.style.visibility = "visible"
-            if (schedule.style.display == "block") {
-                schedule.style.display = "none"
-            } else {schedule.style.display = "block"}
-        } else {
-            schedule.style.display = "block"
-            if (schedule.style.visibility == "visible") {
-                schedule.style.visibility = "hidden"
-            } else {schedule.style.visibility = "visible"}
-        }
-    })
+    
     
     switch (currentDay) { // to show the current schedule
         case 1:
             currentSchedule.innerHTML = "Monday 11:00-23:00"
-            scheduleParagraphs[1].style.border = "solid 4px black"
             scheduleParagraphs[1].style.background = "rgb(240,240,240)"
             break
         case 2:
             currentSchedule.innerHTML = "Tuesday 11:00-23:00"
-            scheduleParagraphs[2].style.border = "solid 4px black"
             scheduleParagraphs[2].style.background = "rgb(240,240,240)"
             break
         case 3:
             currentSchedule.innerHTML = "Wednesday 11:00-23:00"
-            scheduleParagraphs[3].style.border = "solid 4px black"
             scheduleParagraphs[3].style.background = "rgb(240,240,240)"
             break
         case 4:
             currentSchedule.innerHTML = "Thursday 11:00-23:00"
-            scheduleParagraphs[4].style.border = "solid 4px black"
             scheduleParagraphs[4].style.background = "rgb(240,240,240)"
             break
         case 5:
             currentSchedule.innerHTML = "Friday 11:00-00:00"
-            scheduleParagraphs[5].style.border = "solid 4px black"
             scheduleParagraphs[5].style.background = "rgb(240,240,240)"
             break
         case 6:
             currentSchedule.innerHTML = "Saturday 11:00-00:00"
-            scheduleParagraphs[6].style.border = "solid 4px black"
             scheduleParagraphs[6].style.background = "rgb(240,240,240)"
             break
         default:
             currentSchedule.innerHTML = "Sunday 11:00-23:00"
-            scheduleParagraphs[0].style.border = "solid 4px black"
             scheduleParagraphs[0].style.background = "rgb(240,240,240)"
             break;
     }
--- js/order.js
+++ js/order.js
@@ -1,12 +1,38 @@
 let productCategoriesButtons = []
 let productCategoriesHr = []
 let productCategoriesDivs = []
-
 let currentProductDelete = []
 let addToOrderButton = []
+let productQuantity = []
+
 let productsDiv
+let currentOrderPriceText
 
 let productCategorySelected
+let currentOrderPrice
+
+    /* Current Order Products Info */
+let currentOrderProduct = [ // currentOrderProduct.filter((e) => e.existence == true) to get the all current products info
+    {
+        existence: false,
+        quantity: 0,
+        name: null,
+        unitPrice: 0,
+        totalPrice: 0,
+        structure: 
+        {
+            mainDiv: null,
+            mainP: null,
+            nameSpan: null,
+            priceSpan: null,
+            quantityDiv: null,
+            quantitySpan: null,
+            quantityLess: null,
+            quantityMore: null,
+            delete: null
+        }
+    }
+]
 
 
 document.addEventListener("DOMContentLoaded",()=>{
@@ -14,10 +40,12 @@ document.addEventListener("DOMContentLoaded",()=>{
 
     productCategoriesButtons = document.getElementsByClassName("product-categories")[0].getElementsByTagName("button")
     productCategoriesDivs = document.getElementsByClassName("product-categories-divs")[0].children
-    
     currentProductDelete = document.getElementsByClassName("delete-current-product")
     addToOrderButton = document.getElementsByClassName("add-to-order")
+    productQuantity = document.getElementsByClassName("product-quantity")
+
     productsDiv = document.getElementsByClassName("products-position")[0]
+    currentOrderPriceText = document.getElementsByClassName("current-price")[0]
 
     productCategorySelected = document.getElementById("current-category")
 
@@ -70,44 +98,162 @@ document.addEventListener("DOMContentLoaded",()=>{
     }
 
 
-            /* current product deletion */
-    Array.from(currentProductDelete).forEach(e => { // from HTMLCollection to Array
-        ParentElementDeletion(e)
+            /* if a user writes manually a number<1 the value changes automatically to 1 */
+    Array.from(productQuantity).forEach(e => {
+        e.addEventListener("change",()=>{if (e.value<1){e.value=1}})
     })
     
 
 
             /* automate current products */
-    for (let b = 0; b<addToOrderButton.length; b++) {
+    for (let b = 0; b<addToOrderButton.length; b++) { // change the let variables to Array
+        currentOrderProduct[b] = 
+        {
+            existence: false,
+            quantity: 0,
+            name: null,
+            unitPrice: 0,
+            totalPrice: 0,
+            structure: 
+            {
+                mainDiv: null,
+                mainP: null,
+                nameSpan: null,
+                priceSpan: null,
+                quantityDiv: null,
+                quantitySpan: null,
+                quantityLess: null,
+                quantityMore: null,
+                delete: null
+            }
+        }
 
         addToOrderButton[b].addEventListener("click",()=>{
-            
-            let newDiv = productsDiv.appendChild(document.createElement("div"))
-            newDiv.setAttribute("class","current-product")
-            let newP = newDiv.appendChild(document.createElement("p"))
-            let newQuantitySpan = newP.appendChild(document.createElement("span"))
-            newQuantitySpan.setAttribute("class","current-product-quantity")
-            let newNameSpan = newP.appendChild(document.createElement("span"))
-            newNameSpan.setAttribute("class","current-product-name")
-            let newDelete = newDiv.appendChild(document.createElement("div"))
-            newDelete.setAttribute("class","delete-current-product")
-            ParentElementDeletion(newDelete) // current added product deletion
-            let newImg = newDelete.appendChild(document.createElement("img"))
-            newImg.setAttribute("src","../Images/delete.png")
-            newImg.setAttribute("alt","delete")
-            newQuantitySpan.textContent = "quantityTest "
-            newNameSpan.textContent = " nameTest"
+
+            if (!currentOrderProduct[b].existence) 
+            {
+                
+                currentOrderProduct[b].existence = true
+                currentOrderProduct[b].name = addToOrderButton[b].parentElement.parentElement.children[0].children[0].textContent
+                currentOrderProduct[b].quantity = Number.parseInt(addToOrderButton[b].parentElement.children[0].value)
+                currentOrderProduct[b].unitPrice = Number.parseFloat(addToOrderButton[b].parentElement.parentElement.children[0].children[2].textContent.replace("€",""))
+                currentOrderProduct[b].totalPrice = currentOrderProduct[b].unitPrice*currentOrderProduct[b].quantity
+
+                currentOrderProduct[b].structure.mainDiv = productsDiv.appendChild(document.createElement("div"))
+                currentOrderProduct[b].structure.mainDiv.setAttribute("class","current-product")
+
+                currentOrderProduct[b].structure.mainP = currentOrderProduct[b].structure.mainDiv.appendChild(document.createElement("p"))
+
+                currentOrderProduct[b].structure.nameSpan = currentOrderProduct[b].structure.mainP.appendChild(document.createElement("span"))
+                currentOrderProduct[b].structure.nameSpan.setAttribute("class","current-product-name")
+
+                currentOrderProduct[b].structure.priceSpan = currentOrderProduct[b].structure.mainP.appendChild(document.createElement("span"))
+                currentOrderProduct[b].structure.priceSpan.setAttribute("class","current-product-price")
+
+                
+                currentOrderProduct[b].structure.quantityDiv = currentOrderProduct[b].structure.mainDiv.appendChild(document.createElement("div"))
+                currentOrderProduct[b].structure.quantityDiv.setAttribute("class","current-product-quantity-div")
+
+                currentOrderProduct[b].structure.quantityLess = currentOrderProduct[b].structure.quantityDiv.appendChild(document.createElement("div"))
+                currentOrderProduct[b].structure.quantityLess.setAttribute("class","current-product-quantity-less")
+                let newQuantityLessImg = currentOrderProduct[b].structure.quantityLess.appendChild(document.createElement("img"))
+                newQuantityLessImg.setAttribute("src","../Images/less.png")
+                newQuantityLessImg.setAttribute("alt","less")
+
+                currentOrderProduct[b].structure.quantitySpan = currentOrderProduct[b].structure.quantityDiv.appendChild(document.createElement("div"))
+                currentOrderProduct[b].structure.quantitySpan.setAttribute("class","current-product-quantity-info")
+
+                currentOrderProduct[b].structure.quantityMore = currentOrderProduct[b].structure.quantityDiv.appendChild(document.createElement("div"))
+                currentOrderProduct[b].structure.quantityMore.setAttribute("class","current-product-quantity-more")
+                let newQuantityMoreImg = currentOrderProduct[b].structure.quantityMore.appendChild(document.createElement("img"))
+                newQuantityMoreImg.setAttribute("src","../Images/more.png")
+                newQuantityMoreImg.setAttribute("alt","more")
+                
+                currentOrderProduct[b].structure.delete = currentOrderProduct[b].structure.mainDiv.appendChild(document.createElement("div"))
+                currentOrderProduct[b].structure.delete.setAttribute("class","delete-current-product")
+                let newImgDelete = currentOrderProduct[b].structure.delete.appendChild(document.createElement("img"))
+                newImgDelete.setAttribute("src","../Images/delete.png")
+                newImgDelete.setAttribute("alt","delete")
+
+                currentOrderProduct[b].structure.quantityLess.addEventListener("click", () =>
+                    {
+                        if (currentOrderProduct[b].quantity>1)
+                            {
+                                currentOrderProduct[b].quantity--
+                                CurrentOrderProductUpdate(currentOrderProduct[b])
+                                CurrentOrderPriceFunction()
+                            }
+                    })
+                currentOrderProduct[b].structure.quantityMore.addEventListener("click", () =>
+                    {
+                        currentOrderProduct[b].quantity++
+                        CurrentOrderProductUpdate(currentOrderProduct[b])
+                        CurrentOrderPriceFunction()
+                    })
+                currentOrderProduct[b].structure.delete.addEventListener("click", () =>
+                    {
+                        currentOrderProduct[b].structure.delete.parentElement.remove()
+                        currentOrderProduct[b].existence = false
+                        EmptyOrder()
+                    }
+            ) // current added product deletion
+                
+                
+            } 
+            else 
+            {
+                currentOrderProduct[b].quantity += Number.parseInt(addToOrderButton[b].parentElement.children[0].value)
+            }
+
+            EmptyOrder()
+            CurrentOrderProductUpdate(currentOrderProduct[b])
+            CurrentOrderPriceFunction()
+
+            addToOrderButton[b].parentElement.children[0].value = 1
 
         })
-        
+
     }
 
-
+    EmptyOrder()
 })
 
-ParentElementDeletion = (e) => {
-    e.addEventListener("click", () => {e.parentElement.remove()}, false)
+
+EmptyOrder = () => {
+    if (!productsDiv.childElementCount>0) {
+        for (let e = 0; e<productsDiv.parentElement.childElementCount; e++){
+            productsDiv.parentElement.children[e].style.display = "none"
+        }
+        productsDiv.parentElement.children[0].style.display = "block"
+    }
+    else {
+        for (let e = 0; e<productsDiv.parentElement.childElementCount; e++){
+            productsDiv.parentElement.children[e].style.display = "flex"
+        }
+        productsDiv.parentElement.children[0].style.display = "none"
+    }
 }
+
+CurrentOrderPriceFunction = () => {
+    currentOrderPrice = 0
+
+    let currentOrderProductExist = currentOrderProduct.filter((e) => e.existence == true)
+
+    for (let e = 0; e<currentOrderProductExist.length; e++)
+        {
+            currentOrderPrice += currentOrderProductExist[e].totalPrice
+        }
+
+        currentOrderPriceText.textContent = currentOrderPrice.toFixed(2)+"€"
+}
+
+CurrentOrderProductUpdate = (e) => {
+    e.totalPrice = e.unitPrice*e.quantity
+    e.structure.nameSpan.textContent = e.name
+    e.structure.priceSpan.textContent = e.totalPrice+"€"
+    e.structure.quantitySpan.textContent = e.quantity
+}
+
 
 /*              Other way for current product deletion:
 
