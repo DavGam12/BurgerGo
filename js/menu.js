b4d6c299658bb322371b2e5df1023a6baa0d684c
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2024-05-16 17:21:35 +0200

    Product automation structure done without the allergies.

--- js/menu.js
+++ js/menu.js
@@ -78,3 +78,58 @@ document.addEventListener("DOMContentLoaded", () => {
     })
     
 })
+
+
+/* FETCH */
+const burgersURL = "http://localhost:8080/BurgerGo/Controller?action=products.burgers"
+const kidsURL = "http://localhost:8080/BurgerGo/Controller?action=products.kids"
+const dessertsURL = "http://localhost:8080/BurgerGo/Controller?action=products.desserts"
+const drinksURL = "http://localhost:8080/BurgerGo/Controller?action=products.drinks"
+const othersURL = "http://localhost:8080/BurgerGo/Controller?action=products.others"
+
+const fetchData = async () => {
+    const res = await fetch(burgersURL)
+    const data = await res.json()
+    console.log("api data --> ", data)
+    printData(data)
+}
+
+/* BURGERS */
+const printData = (data) => {
+    Array.from(data).forEach(e => {
+        const burgerCategory = document.getElementById("burgers-category")
+
+        const menuProductDiv = document.createElement("div")
+        burgerCategory.appendChild(menuProductDiv)
+        menuProductDiv.classList.add("menu-product")
+
+        const productImg = document.createElement("div")
+        menuProductDiv.appendChild(productImg)
+        productImg.classList.add("product-img")
+        productImg.style.backgroundImage = "url("+e._productImg+")"
+        const productInfo = document.createElement("div")
+        menuProductDiv.appendChild(productInfo)
+        productInfo.classList.add("product-info")
+
+        const productName = document.createElement("p")
+        productInfo.appendChild(productName)
+        productName.classList.add("product-name")
+        productName.textContent = e._productName
+        const productDescription = document.createElement("p")
+        productInfo.appendChild(productDescription)
+        productDescription.classList.add("product-description")
+        productDescription.textContent = e._productDescription
+        const productPrice = document.createElement("p")
+        productInfo.appendChild(productPrice)
+        productPrice.classList.add("product-price")
+        productPrice.textContent = e._productPrice
+        const productAllergies = document.createElement("div")
+        productInfo.appendChild(productAllergies)
+        productAllergies.classList.add("product-allergies")
+        /*Array.from().forEach(a => {
+            const 
+        })*/
+    })
+}
+
+fetchData()
