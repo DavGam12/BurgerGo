bc86c5fab080d99d94d042a833f205a4b922fec8
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2024-05-16 16:42:08 +0200

    Added all the posible categories to the products dao and action.

--- java/src/main/java/Model/IDao/ProductsDao.java
+++ java/src/main/java/Model/IDao/ProductsDao.java
@@ -8,7 +8,13 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class ProductsDao implements IDao {
-    private final String SQL_FIND_ALL = "select * from employees where 1=1";
+    private final String SQL_FIND_ALL = "select * from products where 1=1";
+    private final String SQL_FIND_BURGERS = "select * from products prod inner join categories cat on prod.category_id = cat.category_id where 1=1 and lower(category_name) = 'burgers'";
+    private final String SQL_FIND_KIDS = "select * from products prod inner join categories cat on prod.category_id = cat.category_id where 1=1 and lower(category_name) = 'kids'";
+    private final String SQL_FIND_DESSERTS = "select * from products prod inner join categories cat on prod.category_id = cat.category_id where 1=1 and lower(category_name) = 'desserts'";
+    private final String SQL_FIND_DRINKS = "select * from products prod inner join categories cat on prod.category_id = cat.category_id where 1=1 and lower(category_name) = 'drinks'";
+    private final String SQL_FIND_OTHERS = "select * from products prod inner join categories cat on prod.category_id = cat.category_id where 1=1 and lower(category_name) = 'others'";
+    private final String SQL_FIND_GLUTEN_FREE = "select * from products prod inner join categories cat on prod.category_id = cat.category_id where 1=1 and lower(category_name) = 'gluten free'";
     @Override
     public int add(Object o) {return 0;}
 
@@ -24,8 +30,176 @@ public class ProductsDao implements IDao {
         SQLMotor motor = new SQLMotor();
         try {
             motor.connect();
-            String sql = SQL_FIND_ALL;
+            // String sql = SQL_FIND_ALL;
             ResultSet rs = motor.executeQuery(SQL_FIND_ALL);
+
+
+            while (rs.next()){
+                Products product = new Products();
+                product.setProductID(rs.getString("product_id".toUpperCase()));
+                product.setProductName(rs.getString("product_name".toUpperCase()));
+                product.setProductImg(rs.getString("product_img".toUpperCase()));
+                product.setProductDescription(rs.getString("product_description".toUpperCase()));
+                product.setProductPrice(rs.getFloat("product_price".toUpperCase()));
+                product.setCategoryID(rs.getString("category_id".toUpperCase()));
+
+                products.add(product);
+            }
+        } catch (SQLException sqlEx) {
+            products.clear();
+        } finally {
+            motor.disconnect();
+        }
+        return products;
+    }
+
+    public ArrayList<Products> findBurgers(Object o) {
+        ArrayList<Products> products = new ArrayList<>();
+        SQLMotor motor = new SQLMotor();
+        try {
+            motor.connect();
+            // String sql = SQL_FIND_BURGERS;
+            ResultSet rs = motor.executeQuery(SQL_FIND_BURGERS);
+
+
+            while (rs.next()){
+                Products product = new Products();
+                product.setProductID(rs.getString("product_id".toUpperCase()));
+                product.setProductName(rs.getString("product_name".toUpperCase()));
+                product.setProductImg(rs.getString("product_img".toUpperCase()));
+                product.setProductDescription(rs.getString("product_description".toUpperCase()));
+                product.setProductPrice(rs.getFloat("product_price".toUpperCase()));
+                product.setCategoryID(rs.getString("category_id".toUpperCase()));
+
+                products.add(product);
+            }
+        } catch (SQLException sqlEx) {
+            products.clear();
+        } finally {
+            motor.disconnect();
+        }
+        return products;
+    }
+
+    public ArrayList<Products> findKids(Object o) {
+        ArrayList<Products> products = new ArrayList<>();
+        SQLMotor motor = new SQLMotor();
+        try {
+            motor.connect();
+            // String sql = SQL_FIND_KIDS;
+            ResultSet rs = motor.executeQuery(SQL_FIND_KIDS);
+
+
+            while (rs.next()){
+                Products product = new Products();
+                product.setProductID(rs.getString("product_id".toUpperCase()));
+                product.setProductName(rs.getString("product_name".toUpperCase()));
+                product.setProductImg(rs.getString("product_img".toUpperCase()));
+                product.setProductDescription(rs.getString("product_description".toUpperCase()));
+                product.setProductPrice(rs.getFloat("product_price".toUpperCase()));
+                product.setCategoryID(rs.getString("category_id".toUpperCase()));
+
+                products.add(product);
+            }
+        } catch (SQLException sqlEx) {
+            products.clear();
+        } finally {
+            motor.disconnect();
+        }
+        return products;
+    }
+
+    public ArrayList<Products> findDesserts(Object o) {
+        ArrayList<Products> products = new ArrayList<>();
+        SQLMotor motor = new SQLMotor();
+        try {
+            motor.connect();
+            // String sql = SQL_FIND_DESSERTS;
+            ResultSet rs = motor.executeQuery(SQL_FIND_DESSERTS);
+
+
+            while (rs.next()){
+                Products product = new Products();
+                product.setProductID(rs.getString("product_id".toUpperCase()));
+                product.setProductName(rs.getString("product_name".toUpperCase()));
+                product.setProductImg(rs.getString("product_img".toUpperCase()));
+                product.setProductDescription(rs.getString("product_description".toUpperCase()));
+                product.setProductPrice(rs.getFloat("product_price".toUpperCase()));
+                product.setCategoryID(rs.getString("category_id".toUpperCase()));
+
+                products.add(product);
+            }
+        } catch (SQLException sqlEx) {
+            products.clear();
+        } finally {
+            motor.disconnect();
+        }
+        return products;
+    }
+
+    public ArrayList<Products> findDrinks(Object o) {
+        ArrayList<Products> products = new ArrayList<>();
+        SQLMotor motor = new SQLMotor();
+        try {
+            motor.connect();
+            // String sql = SQL_FIND_DRINKS;
+            ResultSet rs = motor.executeQuery(SQL_FIND_DRINKS);
+
+
+            while (rs.next()){
+                Products product = new Products();
+                product.setProductID(rs.getString("product_id".toUpperCase()));
+                product.setProductName(rs.getString("product_name".toUpperCase()));
+                product.setProductImg(rs.getString("product_img".toUpperCase()));
+                product.setProductDescription(rs.getString("product_description".toUpperCase()));
+                product.setProductPrice(rs.getFloat("product_price".toUpperCase()));
+                product.setCategoryID(rs.getString("category_id".toUpperCase()));
+
+                products.add(product);
+            }
+        } catch (SQLException sqlEx) {
+            products.clear();
+        } finally {
+            motor.disconnect();
+        }
+        return products;
+    }
+
+    public ArrayList<Products> findOthers(Object o) {
+        ArrayList<Products> products = new ArrayList<>();
+        SQLMotor motor = new SQLMotor();
+        try {
+            motor.connect();
+            // String sql = SQL_FIND_OTHERS;
+            ResultSet rs = motor.executeQuery(SQL_FIND_OTHERS);
+
+
+            while (rs.next()){
+                Products product = new Products();
+                product.setProductID(rs.getString("product_id".toUpperCase()));
+                product.setProductName(rs.getString("product_name".toUpperCase()));
+                product.setProductImg(rs.getString("product_img".toUpperCase()));
+                product.setProductDescription(rs.getString("product_description".toUpperCase()));
+                product.setProductPrice(rs.getFloat("product_price".toUpperCase()));
+                product.setCategoryID(rs.getString("category_id".toUpperCase()));
+
+                products.add(product);
+            }
+        } catch (SQLException sqlEx) {
+            products.clear();
+        } finally {
+            motor.disconnect();
+        }
+        return products;
+    }
+
+    public ArrayList<Products> findGlutenFree(Object o) {
+        ArrayList<Products> products = new ArrayList<>();
+        SQLMotor motor = new SQLMotor();
+        try {
+            motor.connect();
+            // String sql = SQL_FIND_GLUTEN_FREE;
+            ResultSet rs = motor.executeQuery(SQL_FIND_GLUTEN_FREE);
 
 
             while (rs.next()){
