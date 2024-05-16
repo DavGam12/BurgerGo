59efe883b5b7c45060af9a7796988832c0b0cc18
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2024-05-16 18:32:36 +0200

    Automated all the product structure in the Gluten Free and Menu Pages without the allergies.

--- js/gluten-free.js
+++ js/gluten-free.js
@@ -23,3 +23,57 @@ document.addEventListener("DOMContentLoaded", () => {
     })
 })
 
+
+
+/* FETCH */
+const glutenFreeURL = "http://localhost:8080/BurgerGo/Controller?action=products.gluten-free"
+
+const fetchData = async () => {
+    const glutenFreeRes = await fetch(glutenFreeURL)
+
+    const glutenFreeData = await glutenFreeRes.json()
+
+    console.log("glutenFreeData --> ", glutenFreeData)
+
+    printGlutenFreeData(glutenFreeData)
+}
+
+const printGlutenFreeData = (data) => {
+    Array.from(data).forEach(e => {
+        const glutenFreeCategory = document.getElementById("gluten-free-category")
+
+        const menuProductDiv = document.createElement("div")
+        glutenFreeCategory.appendChild(menuProductDiv)
+        menuProductDiv.classList.add("menu-product")
+
+        const productImg = document.createElement("div")
+        menuProductDiv.appendChild(productImg)
+        productImg.classList.add("product-img")
+        productImg.style.backgroundImage = "url("+e._productImg+")"
+        const productInfo = document.createElement("div")
+        menuProductDiv.appendChild(productInfo)
+        productInfo.classList.add("product-info")
+
+        const productName = document.createElement("p")
+        productInfo.appendChild(productName)
+        productName.classList.add("product-name")
+        productName.textContent = e._productName
+        const productDescription = document.createElement("p")
+        productInfo.appendChild(productDescription)
+        productDescription.classList.add("product-description")
+        productDescription.textContent = e._productDescription
+        const productPrice = document.createElement("p")
+        productInfo.appendChild(productPrice)
+        productPrice.classList.add("product-price")
+        productPrice.textContent = e._productPrice+"$"
+        const productAllergies = document.createElement("div")
+        productInfo.appendChild(productAllergies)
+        productAllergies.classList.add("product-allergies")
+        /*Array.from().forEach(a => {
+            const 
+        })*/
+    })
+}
+
+
+fetchData()
