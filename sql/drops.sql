5b0bb8613dd55879e3219e89415c1bf0e055b89d
RobertoGm1333 <157628987+RobertoGm1333@users.noreply.github.com>
2024-05-20 19:49:16 +0200

    Drops y creates con las secuencias

---
+++ sql/drops.sql
@@ -0,0 +1,13 @@
+drop sequence customer_id;
+drop sequence order_id ;
+drop sequence detail_id;
+
+drop table details;
+drop table allergies;
+drop table products;
+drop table categories;
+drop table allergens;
+drop table orders;
+drop table employees;
+drop table jobs;
+drop table customers;
