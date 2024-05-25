f2106656cf541fd1ca7fb212cd8f88c0c09f2bf1
David <154616047+DavGam12@users.noreply.github.com>
2024-05-25 18:18:07 +0200

    Added all the selects of the Daos

--- sql/selects.sql
+++ sql/selects.sql
@@ -1,18 +1,17 @@
-select table_name from user_tables;
-
-/*
-    allergens = gens
-    allergies = gies
-    products = prod
-    sales_management = smanage
-    sales = sal
-    categories = cat
-    details = det
-    orders = ord
-    customers = cust
-    employees = em
-    jobs = jb
-*/
-
-select * from allergens gens inner join allergies gies on gens.allergen_id = allergy_id inner join products prod on prod.product_id = gies.product_id inner join sales_management smanage on prod.product_id = smanage.product_id inner join sales sal on sal.sale_id = smanage.sale_id inner join categories cat on cat.category_id = prod.category_id inner join details det on prod.product_id = det.product_id inner join orders ord on ord.order_id = det.order_id inner join customers cust on cust.customer_id = ord.customer_id inner join employees em on em.employee_id = ord.employee_id inner join jobs jb on jb.job_id = em.job_id;
-
+select * from allergens order by allergen_id; -- ALLERGENS DAO FIND ALL SELECT
+select * from allergies order by allergy_id; -- ALLERGIES DAO FIND ALL SELECT
+select * from categories order by category_id; -- CATEGORIES DAO FIND ALL SELECT
+select * from customers order by customer_id; -- CUSTOMERS DAO FIND ALL SELECT
+select * from details order by detail_id; -- DETAILS DAO FIND ALL SELECT
+select * from details where product_id=(product_id); -- DETAILS DAO FIND SPECIFIC SELECT
+select * from employees order by employee_id; -- EMPLOYEES DAO FIND ALL SELECT
+select * from jobs order by job_id; -- JOBS DAO FIND ALL SELECT
+select * from orders order by order_id; -- ORDERS DAO FIND ALL SELECT
+select * from orders where order_state=(order_state) and customer_id=(customer_id); -- ORDERS DAO FIND SPECIFIC SELECT
+select * from products order by product_id; -- PRODUCTS DAO FIND ALL SELECT
+select * from products prod inner join categories cat on prod.category_id = cat.category_id where 1=1 and lower(category_name) like '%burgers%' order by product_id; -- PRODUCTS DAO FIND ALL PRODUCTS FROM BURGERS CATEGORY SELECT
+select * from products prod inner join categories cat on prod.category_id = cat.category_id where 1=1 and lower(category_name) like '%kids%' order by product_id; -- PRODUCTS DAO FIND ALL PRODUCTS FROM KIDS CATEGORY SELECT
+select * from products prod inner join categories cat on prod.category_id = cat.category_id where 1=1 and lower(category_name) like '%desserts%' order by product_id; -- PRODUCTS DAO FIND ALL PRODUCTS FROM DESSERTS CATEGORY SELECT
+select * from products prod inner join categories cat on prod.category_id = cat.category_id where 1=1 and lower(category_name) like '%drinks%' order by product_id; -- PRODUCTS DAO FIND ALL PRODUCTS FROM DRINKS CATEGORY SELECT
+select * from products prod inner join categories cat on prod.category_id = cat.category_id where 1=1 and lower(category_name) like '%others%' order by product_id; -- PRODUCTS DAO FIND ALL PRODUCTS FROM OTHERS CATEGORY SELECT
+select * from products prod inner join categories cat on prod.category_id = cat.category_id where 1=1 and lower(category_name) like '%gluten%free%' order by product_id; -- PRODUCTS DAO FIND ALL PRODUCTS FROM GLUTEN FREE CATEGORY SELECT
