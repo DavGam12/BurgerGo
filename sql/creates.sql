e934f9e2a4e7430bc5cf97860bfbdbe47721a414
David <154616047+DavGam12@users.noreply.github.com>
2024-05-26 12:54:39 +0200

    Added unique attribute to the emails

--- sql/creates.sql
+++ sql/creates.sql
@@ -3,11 +3,11 @@ create sequence order_id start with 1 increment by 1 nocache;
 create sequence detail_id start with 1 increment by 1 nocache;
 
 
-create table customers(customer_id number(8) primary key, first_name varchar2(40) not null, last_name varchar2(60), email varchar2(50) not null, phone_number varchar2(13) not null, password varchar2(40) not null);
+create table customers(customer_id number(8) primary key, first_name varchar2(40) not null, last_name varchar2(60), email varchar2(50) unique not null, phone_number varchar2(13) not null, password varchar2(40) not null);
 
 create table jobs(job_id number(2) primary key, job_name varchar2(40) not null);
 
-create table employees(employee_id number(3) primary key, first_name varchar2(40) not null, last_name varchar2(60) not null, email varchar2(50) not null, ss_number varchar2(12) not null, birth_date varchar2(12) not null, hire_date varchar2(12) not null, salary number(6,2) not null, permission number(1,0) not null, password varchar2(40) not null, job_id number(2), foreign key(job_id) references jobs(job_id));
+create table employees(employee_id number(3) primary key, first_name varchar2(40) not null, last_name varchar2(60) not null, email varchar2(50) unique not null, ss_number varchar2(12) not null, birth_date varchar2(12) not null, hire_date varchar2(12) not null, salary number(6,2) not null, permission number(1,0) not null, password varchar2(40) not null, job_id number(2), foreign key(job_id) references jobs(job_id));
 
 create table orders(order_id number(10) primary key, order_state varchar2(40) not null, direction varchar2(100) not null, order_price number(6,2) not null, order_date varchar2(12) not null, employee_id number(3), foreign key(employee_id) references employees(employee_id), customer_id number(6), foreign key(customer_id) references customers(customer_id));
 
